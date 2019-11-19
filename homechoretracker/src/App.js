8ee0d47b414fe776d25ff2f854c89c8f0bc4a06a
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2019-11-19 10:42:36 -0800

    did forms for sign up and adding a chore, rough draft

--- homechoretracker/src/App.js
+++ homechoretracker/src/App.js
@@ -3,16 +3,24 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './components/Login';
 import PrivateRoute from "./components/PrivateRoute";
 import Parent from "./components/Parent";
+import Chores from "./components/Chores";
 import './App.css';
 
 function App() {
+
+  
+
   return (
+  
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
         <PrivateRoute exact path="/protected" component={Parent} />
+        <Route path="/signup" component={Parent} />
+        <Route path="/chores" component={Chores} />
       </div>
     </Router>
+  
   );
 }
 
--- homechoretracker/src/components/Parent.js
+++ homechoretracker/src/components/Parent.js
@@ -1,9 +1,92 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default function Parent() {
+
+
+  // Form and form styling
+    const useStyles = makeStyles(theme => ({
+        container: {
+          display: 'flex',
+          flexDirection: 'column',
+        },
+        textField: {
+          marginLeft: theme.spacing(1),
+          marginRight: theme.spacing(1),
+          width: 300,
+        },
+        button: {
+            margin: theme.spacing(1),
+          },
+          input: {
+            display: 'none',
+          },
+      }));
+      const classes = useStyles();
+
     return (
-        <div>
-            <h1> Hello Parents</h1>
+        <form className={classes.container} noValidate autoComplete="off">
+        
+            <div className={classes.form}>
+                <div>
+                <TextField
+                    id="outlined-basic"
+                    className={classes.textField}
+                    label="First Name"
+                    margin="normal"
+                    variant="outlined"
+                />
+                </div>
+                <div>
+                <TextField
+                    id="outlined-basic"
+                    className={classes.textField}
+                    label="Last Name"
+                    margin="normal"
+                    variant="outlined"
+                />
+                </div>
+                <div>
+                <TextField
+                    id="outlined-basic"
+                    className={classes.textField}
+                    label="Username"
+                    margin="normal"
+                    variant="outlined"
+                />
+                </div>
+                <div>
+                <TextField
+                    id="outlined-basic"
+                    className={classes.textField}
+                    label="Email"
+                    type="email"
+                    margin="normal"
+                    variant="outlined"
+                    
+                />
+                </div>
+                <div>
+                    <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    className={classes.textField}
+                    type="password"
+                    autoComplete="current-password"
+                    margin="normal"
+                    variant="outlined"
+                    />
+                </div>
+                <Button variant="contained" className={classes.button}>
+                    Sign Up
+                </Button>
         </div>
+      </form>
     )
 }
+
+
+
+ 
