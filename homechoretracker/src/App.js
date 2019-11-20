1103de502be434acfb97e3f53899577808dcc92b
nicanor-alicaya <donalicaya@gmail.com>
2019-11-20 17:57:57 -0500

    children modified

--- homechoretracker/src/App.js
+++ homechoretracker/src/App.js
@@ -1,37 +1,34 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-import Login from './components/Login';
+import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import Parent from "./components/Parent";
 import Chores from "./components/Chores";
-import './App.css';
+import "./App.css";
 import Children from "./components/Children";
 
 function App() {
-
-
-
-  return (
-
-    <Router>
-      <div className="App">
-        <header>
-          <Link to="/login">Login</Link><br />
-          {/* <Link to="protected">Parent</Link> */}
-          <Link to="/children">Children</Link>
-        </header>
-        <Switch>
-          <Route exact path="/" component={Parent} />
-          <PrivateRoute exact path="/protected" component={Children} />
-          {/* <Route path="/protected" component={Children} /> */}
-
-          <Route path="/chores" component={Chores} />
-        </Switch>
-      </div>
-    </Router>
-
-  );
+	return (
+		<Router>
+			<div className="App">
+				<header>
+					<Link to="/login">Login</Link>
+					<br />
+					{/* <Link to="protected">Parent</Link> */}
+					<Link to="/children">Children</Link>
+				</header>
+				<Switch>
+					<Route exact path="/" component={Parent} />
+					<Route exact path="/children" component={Children} />
+					{/* <Route path="/protected" component={Children} /> */}
+          <Route path="/signup" component={Parent} />
+          
+					<Route path="/chores" component={Chores} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- homechoretracker/src/components/Children.js
+++ homechoretracker/src/components/Children.js
@@ -1,9 +1,151 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axiosWithAuth from "./axiosWithAuth";
 
 export default function Children() {
-    return (
-        <div>
+	//styling
+	const useStyles = makeStyles(theme => ({
+		container: {
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			flexWrap: "wrap"
+		},
 
-        </div>
-    )
+		textField: {
+			marginLeft: theme.spacing(1),
+			marginRight: theme.spacing(1),
+			width: 200
+		},
+
+		button: {
+			margin: theme.spacing(1),
+			width: "10%"
+		},
+
+		input: {
+			display: "none"
+		}
+	}));
+
+	const classes = useStyles();
+
+	const [children, setChildren] = useState([
+		{
+            child_username: "",
+            child_password:"",
+			child_id: "",
+			parent_id: "",
+			chore_score: "",
+			chore_streak: "",
+			role: "child"
+		}
+	]);
+// console.log(children)
+	useEffect(() => {
+		axiosWithAuth()
+			.get('https://chore-tracker-bw.herokuapp.com/children/')
+			.then(res => {
+				console.log(res);
+				setChildren(res.data);
+			})
+			.catch(err => console.log("Check ERROR IN CHILDREN", err));
+	}, []);
+
+	const handleChanges = e => {
+		setChildren({ ...children, [e.target.name]: e.target.value });
+		console.log(children);
+	};
+	const submitForm = e => {
+		e.preventDefault();
+		setChildren([...children]);
+	};
+
+	return (
+		<form
+			onSubmit={submitForm}
+			className={classes.container}
+			noValidate
+			autoComplete="off"
+		>
+			<div>
+				<TextField
+                    id="child_username"
+                    name='child_username'
+					required
+					className={classes.textField}
+					label="child username"
+					margin="normal"
+					variant="outlined"
+					placeholder="Enter child's username "
+					onChange={handleChanges}
+				/>
+			</div>
+
+			<div>
+				<TextField
+                    id="child_password"
+                    name='child_password'
+					type="password"
+					required
+					className={classes.textField}
+					label="child password"
+					margin="normal"
+					variant="outlined"
+					placeholder="Enter child password "
+					onChange={handleChanges}
+				/>
+			</div>
+
+			<div>
+				<TextField
+                    id="chore_score"
+                    name='chore_score'
+					className={classes.textField}
+					label="chore score"
+					margin="normal"
+					variant="outlined"
+					placeholder="Enter chore score "
+					onChange={handleChanges}
+				/>
+			</div>
+
+			<div>
+				<TextField
+                    id="chore_streak"
+                    name='chore_streak'
+					className={classes.textField}
+					label="chore streak"
+					margin="normal"
+					variant="outlined"
+					placeholder="Enter chore streak "
+					onChange={handleChanges}
+				/>
+			</div>
+
+			<div>
+				<TextField
+                    id="outlined-read-only-input"
+                    name='role'
+					label="Read Only"
+					defaultValue="child"
+					className={classes.textField}
+					margin="normal"
+					InputProps={{
+						readOnly: true
+					}}
+					variant="outlined"
+				/>
+			</div>
+
+			<div>
+				<Button variant="contained" className={classes.button} type="submit">
+					Add
+				</Button>
+			</div>
+		</form>
+	);
 }
