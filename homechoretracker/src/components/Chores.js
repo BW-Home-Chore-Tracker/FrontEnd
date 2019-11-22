0f3a22c3c55a97f092dca4a9a3c09758d27e977b
nicanor-alicaya <donalicaya@gmail.com>
2019-11-22 08:13:29 -0500

    basic styling, edited children.js form

--- homechoretracker/src/components/Chores.js
+++ homechoretracker/src/components/Chores.js
@@ -1,84 +1,90 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
 import ChoreCard from "./ChoreCard";
 
 export default function Chores() {
-    const [chores, setChores] = useState([])
+	const [chores, setChores] = useState([]);
 
-    useEffect(() => {
-        axios.get('https://chore-tracker-bw.herokuapp.com/chores')
-            .then(response => {
-                console.log(response);
-                setChores(response.data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }, [])
+	useEffect(() => {
+		axios
+			.get("https://chore-tracker-bw.herokuapp.com/chores")
+			.then(response => {
+				console.log(response);
+				setChores(response.data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, []);
 
-    const addChore = e => {
-        e.preventDefault();
-        setChores([...chores, { name: "", chore_score: "", due_date: "", completed: "" }]);
-    };
+	const addChore = e => {
+		e.preventDefault();
+		setChores([
+			...chores,
+			{ name: "", chore_score: "", due_date: "", completed: "" }
+		]);
+	};
 
-    // Styling form
+	// Styling form
 
-    const useStyles = makeStyles(theme => ({
-        container: {
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        textField: {
-            marginLeft: theme.spacing(1),
-            marginRight: theme.spacing(1),
-            width: 300,
-        },
-        button: {
-            margin: theme.spacing(1),
-        },
-        input: {
-            display: 'none',
-        },
-    }));
-    const classes = useStyles(1);
+	const useStyles = makeStyles(theme => ({
+		container: {
+			display: "flex",
+			flexDirection: "column"
+		},
+		textField: {
+			marginLeft: theme.spacing(1),
+			marginRight: theme.spacing(1),
+			width: 300
+		},
+		button: {
+			margin: theme.spacing(1)
+		},
+		input: {
+			display: "none"
+		}
+	}));
+	const classes = useStyles(1);
 
-    return (
-        <div>
-            
-         {chores.map(chore  => (
-          <ChoreCard  
-          key={chore.chore_id} 
-          name={chore.name}
-          chore_score={chore.chore_score}
-          due_date={chore.due_date}
-          completed={chore.completed}
-          />
-          )
-        )}
+	return (
+		<div>
+			{chores.map(chore => (
+				<ChoreCard
+					key={chore.chore_id}
+					name={chore.name}
+					chore_score={chore.chore_score}
+					due_date={chore.due_date}
+					completed={chore.completed}
+				/>
+			))}
 
-
-
-            <form name="choreForm" className={classes.container} noValidate autoComplete="off" onSubmit={validateForm} >
-                <div className={classes.form}>
-                    <div>
-                        <TextField
-                            required
-                            id="name"
-                            className={classes.textField}
-                            label="Chore Name"
-                            margin="normal"
-                            variant="outlined"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </div>
-                    <div>
-                        {/* <TextField
+			<form
+				name="choreForm"
+				className={classes.container}
+				noValidate
+				autoComplete="off"
+				onSubmit={validateForm}
+			>
+				<div className={classes.form}>
+					<div>
+						<TextField
+							required
+							id="name"
+							className={classes.textField}
+							label="Chore Name"
+							margin="normal"
+							variant="outlined"
+							InputLabelProps={{
+								shrink: true
+							}}
+						/>
+					</div>
+					<div>
+						{/* <TextField
                             id="description"
                             className={classes.textField}
                             multiline
@@ -90,9 +96,9 @@ export default function Chores() {
                                 shrink: true,
                             }}
                     /> */}
-                    </div>
-                    <div>
-                        {/* <TextField
+					</div>
+					<div>
+						{/* <TextField
                             id="childs_name"
                             className={classes.textField}
                             label="Child's Name"
@@ -102,60 +108,59 @@ export default function Chores() {
                                 shrink: true,
                             }}
                     /> */}
-                    </div>
-                    <div>
-                        <TextField
-                            required
-                            id="chore_score"
-                            className={classes.textField}
-                            label="Chore Score"
-                            type="number"
-                            margin="normal"
-                            variant="outlined"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            required
-                            id="due_date"
-                            label="Due Date"
-                            className={classes.textField}
-                            type="date"
-                            margin="normal"
-                            variant="outlined"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </div>
-                    <div>
-                        <Checkbox
-                            required
-                            name="completed"
-                            color="primary"
-                            inputProps={{
-                                'aria-label': 'secondary checkbox',
-                            }}
-                        /> Completed
-                    </div>
-                    <Button variant="contained" className={classes.button} >
-                        Add Chore
-                    </Button>
-                </div>
-            </form>
-            
-        </div>
-    )
+					</div>
+					<div>
+						<TextField
+							required
+							id="chore_score"
+							className={classes.textField}
+							label="Chore Score"
+							type="number"
+							margin="normal"
+							variant="outlined"
+							InputLabelProps={{
+								shrink: true
+							}}
+						/>
+					</div>
+					<div>
+						<TextField
+							required
+							id="due_date"
+							label="Due Date"
+							className={classes.textField}
+							type="date"
+							margin="normal"
+							variant="outlined"
+							InputLabelProps={{
+								shrink: true
+							}}
+						/>
+					</div>
+					<div>
+						<Checkbox
+							required
+							name="completed"
+							color="primary"
+							inputProps={{
+								"aria-label": "secondary checkbox"
+							}}
+						/>{" "}
+						Completed
+					</div>
+					<Button variant="contained" className={classes.button}>
+						Add Chore
+					</Button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
-
 function validateForm() {
-    const x = document.forms["choreForm"]["name"].value;
-    if (x === "") {
-        alert("Name must be filled out");
-        return false;
-    }
+	const x = document.forms["choreForm"]["name"].value;
+	if (x === "") {
+		alert("Name must be filled out");
+		return false;
+	}
 }
--- homechoretracker/src/components/UpdateChoresForm.js
+++ homechoretracker/src/components/UpdateChoresForm.js
@@ -1,162 +1,162 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import { editChores, deleteChore } from "../actions/actions";
 import { connect } from "react-redux";
-import axiosWithAuth from './axiosWithAuth';
+import axiosWithAuth from "./axiosWithAuth";
 const useStyles = makeStyles(theme => ({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: 300,
-    },
-    button: {
-        margin: theme.spacing(1),
-    },
-    input: {
-        display: 'none',
-    },
+	container: {
+		display: "flex",
+		flexDirection: "column"
+	},
+	textField: {
+		marginLeft: theme.spacing(1),
+		marginRight: theme.spacing(1),
+		width: 300
+	},
+	button: {
+		margin: theme.spacing(1)
+	},
+	input: {
+		display: "none"
+	}
 }));
 
-
 const UpdateChoresForm = props => {
-    const [choreScore, setChoreScore] = useState();
-    const [chores, setChores] = useState({
-        id: new Date(),
-        name: "",
-        due_date: "",
-        created: "",
-        completed: false,
-        editing: false
-    })
+	const [choreScore, setChoreScore] = useState();
+	const [chores, setChores] = useState({
+		id: new Date(),
+		name: "",
+		due_date: "",
+		created: "",
+		completed: false,
+		editing: false
+	});
 
+	const classes = useStyles();
 
-    const classes = useStyles();
+	const deleteChore = e => {
+		e.preventDefault();
+		axiosWithAuth()
+			.delete(`https://chore-tracker-bw.herokuapp.com/chores/:id`)
+			.then(res => {
+				//console.log(res))
+				props.updateChore(res.data);
+				props.history.push("/item-list");
+			})
+			.catch(err => console.log(err));
+	};
+	const handleScore = e => {
+		const scores = choreScore.filter(
+			chore => chores.completed === !chores.completed
+		);
+		setChoreScore(scores.legnth * 10);
+	};
+	const handleSubmit = e => {
+		e.preventDefault();
+		const data = {
+			id: new Date(),
+			name,
+			due_date,
+			created,
+			completed: false,
+			editing: false
+		};
+		const name = this.getName.value;
+		const due_date = data.id.setDate(data.id.getDate() + 5);
+		const created = Date.now();
+		console.log(data);
+	};
 
-
-    const deleteChore = e => {
-        e.preventDefault();
-        axiosWithAuth()
-            .delete(`https://chore-tracker-bw.herokuapp.com/chores/:id`)
-            .then(res => {
-                //console.log(res))
-                props.updateChore(res.data);
-                props.history.push("/item-list");
-            })
-            .catch(err => console.log(err));
-    };
-    const handleScore = e => {
-        const scores = choreScore.filter(chore => chores.completed === !chores.completed)
-        setChoreScore(scores.legnth * 10)
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const data = {
-            id: new Date(),
-            name,
-            due_date,
-            created,
-            completed: false,
-            editing: false
-        }
-        const name = this.getName.value;
-        const due_date = data.id.setDate(data.id.getDate() + 5);
-        const created = Date.now();
-        console.log(data)
-
-    }
-
-
-    return (
-
-
-        <form className={classes.container} noValidate autoComplete="off">
-
-            <div className={classes.form}>
-                <div>
-                    <TextField
-                        id="name"
-                        className={classes.textField}
-                        label="Chore Name"
-                        margin="normal"
-                        variant="outlined"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="description"
-                        className={classes.textField}
-                        multiline
-                        label="Chore Description"
-                        type="textarea"
-                        margin="normal"
-                        variant="outlined"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="childs_name"
-                        className={classes.textField}
-                        label="Child's Name"
-                        margin="normal"
-                        variant="outlined"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="chore_score"
-                        className={classes.textField}
-                        label="Chore Score"
-                        type="number"
-                        margin="normal"
-                        variant="outlined"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="due_date"
-                        label="Due Date"
-                        className={classes.textField}
-                        type="date"
-                        margin="normal"
-                        variant="outlined"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
-                <div>
-                    <Button variant="contained" className={classes.button} onClick={editChores}>
-                        EDIT
-                        </Button>
-                    <Button variant="contained" className={classes.button} onClick={deleteChore}>
-                        DELETE
-                        </Button>
-                </div>
-            </div>
-        </form>
-
-
-
-    )
-}
+	return (
+		<form className={classes.container} noValidate autoComplete="off">
+			<div className={classes.form}>
+				<div>
+					<TextField
+						id="name"
+						className={classes.textField}
+						label="Chore Name"
+						margin="normal"
+						variant="outlined"
+						InputLabelProps={{
+							shrink: true
+						}}
+					/>
+				</div>
+				<div>
+					<TextField
+						id="description"
+						className={classes.textField}
+						multiline
+						label="Chore Description"
+						type="textarea"
+						margin="normal"
+						variant="outlined"
+						InputLabelProps={{
+							shrink: true
+						}}
+					/>
+				</div>
+				<div>
+					<TextField
+						id="childs_name"
+						className={classes.textField}
+						label="Child's Name"
+						margin="normal"
+						variant="outlined"
+						InputLabelProps={{
+							shrink: true
+						}}
+					/>
+				</div>
+				<div>
+					<TextField
+						id="chore_score"
+						className={classes.textField}
+						label="Chore Score"
+						type="number"
+						margin="normal"
+						variant="outlined"
+						InputLabelProps={{
+							shrink: true
+						}}
+					/>
+				</div>
+				<div>
+					<TextField
+						id="due_date"
+						label="Due Date"
+						className={classes.textField}
+						type="date"
+						margin="normal"
+						variant="outlined"
+						InputLabelProps={{
+							shrink: true
+						}}
+					/>
+				</div>
+				<div>
+					<Button
+						variant="contained"
+						className={classes.button}
+						onClick={editChores}
+					>
+						EDIT
+					</Button>
+					<Button
+						variant="contained"
+						className={classes.button}
+						onClick={deleteChore}
+					>
+						DELETE
+					</Button>
+				</div>
+			</div>
+		</form>
+	);
+};
 const mapStateToProps = state => ({ chores: state.chores, error: state.error });
-export default connect(mapStateToProps, { editChores, deleteChore })(UpdateChoresForm);
+export default connect(mapStateToProps, { editChores, deleteChore })(
+	UpdateChoresForm
+);
